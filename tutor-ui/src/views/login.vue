c6fa65db7b41e8847772e1424a161caebd4736a2
zty-f <zruler@163.com>
2022-12-26 11:04:18 +0800

    修改默认密码

--- tutor-ui/src/views/login.vue
+++ tutor-ui/src/views/login.vue
@@ -73,7 +73,7 @@ export default {
       codeUrl: "",
       loginForm: {
         username: "admin",
-        password: "admin123",
+        password: "123456",
         rememberMe: false,
         code: "",
         uuid: ""
