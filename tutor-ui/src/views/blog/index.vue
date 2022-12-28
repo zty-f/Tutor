1e0e47f305dbc17dfc8d53f783496ae8b3aff59a
zty-f <zruler@163.com>
2022-12-28 10:32:46 +0800

    内嵌显示个人博客信息

---
+++ tutor-ui/src/views/blog/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <i-frame :src="url" />
+</template>
+<script>
+import iFrame from "@/components/iFrame/index";
+export default {
+  name: "blog",
+  components: { iFrame },
+  data() {
+    return {
+      url: "https://www.zty-f.fun/"
+    };
+  },
+};
+</script>
